adf5d5613db65c5cf0a47b5b747a5092489bab1a
kylearora <kylearora@gmail.com>
2017-08-08 22:37:15 -0500

    Final Commit with Condensed Javascript Edits

--- main.js
+++ main.js
@@ -2,33 +2,6 @@ let container = document.querySelector('.container')
 let recipe = document.querySelector('.recipe')
 let title = document.querySelector('.title')
 
-fetch("http://recipepuppyproxy.herokuapp.com/api/?i=")
-  .then(function(response){
-    return response.json()
-  }).then(function(data) {
-          console.log(data);
-          let content = ""
-          for (var i = 0; i < data.results.length; i++) {
-              console.log(data.results[i].title);
-
-              let title = `${data.results[i].title}`
-              let ingredients = `${data.results[i].ingredients}`
-              let link = `${data.results[i].href}`
-              let photo = `${data.results[i].thumbnail}`
-
-              content += `
-              <div class = "recipe">
-                <a href ="${link}"><img class = "infoPhoto" src=${photo}></img></a>
-                <a href ="${link}"><span class = "infoTitle"><h2>${title}</h2></span></a>
-                <span class ="infoIngredients"><h2>Ingredients: ${ingredients}</h2></span>
-                <span class ="infoLink"><a href ="${link}"></a></span>
-              </div>
-              `
-          }
-          container.innerHTML = content
-  })
-
-
 let input = document.querySelector('#inputBox').addEventListener("keydown", function(event){
   if (event.keyCode === 13) {
     console.log(event.target.value);
@@ -41,25 +14,31 @@ function getData(event){
     .then(function(response){
       return response.json()
     }).then(function(data) {
-            console.log(data);
-            let content = ""
-            for (var i = 0; i < data.results.length; i++) {
-                console.log(data.results[i].title);
-
-                let title = `${data.results[i].title}`
-                let ingredients = `${data.results[i].ingredients}`
-                let link = `${data.results[i].href}`
-                let photo = `${data.results[i].thumbnail}`
-
-                content += `
-                <div class = "recipe">
-                  <a href ="${link}"><img class = "infoPhoto" src=${photo}></img></a>
-                  <a href ="${link}"><span class = "infoTitle"><h2>${title}</h2></span></a>
-                  <span class ="infoIngredients"><h2>Ingredients: ${ingredients}</h2></span>
-                  <span class ="infoLink"><a href ="${link}"></a></span>
-                </div>
-                `
-            }
-            container.innerHTML = content
+      populateData(data)
     })
   }
+
+getData("")
+
+function populateData(data) {
+  console.log(data);
+  let content = ""
+  for (var i = 0; i < data.results.length; i++) {
+      console.log(data.results[i].title);
+
+      let title = `${data.results[i].title}`
+      let ingredients = `${data.results[i].ingredients}`
+      let link = `${data.results[i].href}`
+      let photo = `${data.results[i].thumbnail}`
+
+      content += `
+      <div class = "recipe">
+        <a href ="${link}"><img class = "infoPhoto" src=${photo}></img></a>
+        <a href ="${link}"><span class = "infoTitle"><h2>${title}</h2></span></a>
+        <span class ="infoIngredients"><h2>Ingredients: ${ingredients}</h2></span>
+        <span class ="infoLink"><a href ="${link}"></a></span>
+      </div>
+      `
+  }
+  container.innerHTML = content
+}
